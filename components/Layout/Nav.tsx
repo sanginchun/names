11688dea2524bf38a31cae98964177010b3cd65b
sanginchun <sanginchun91@gmail.com>
2022-10-12 22:58:35 +0900

    add: Nav

--- components/Layout/Nav.tsx
+++ components/Layout/Nav.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
 import { Menu, Container } from 'semantic-ui-react';
+import { CURRENT_YEAR } from '../../configs';
 
 const Nav = () => {
   return (
     <Menu as="header" borderless fixed="top" inverted size="huge" color="teal">
       <Container as="nav">
         <Link href="/">
-          <a className="header item">인기 이름</a>
+          <a className="header item">홈</a>
+        </Link>
+        <Link href={`/years/${CURRENT_YEAR}?gender=M`}>
+          <a className="header item">연도별 인기 이름</a>
         </Link>
         <Link href="/search">
           <a className="header item">이름 검색</a>
